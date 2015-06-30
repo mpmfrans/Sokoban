b2a7fdac6bb5339674a82de97e6e6a3096d1f3ae
mpmfrans <mpmfrans@gmail.com>
2015-06-30 15:52:04 +0200

    Vakje, soort,richting en krat
    met buren

---
+++ Sokoban/Model/Richting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban.Model
+{
+    enum Richting
+    {
+        NAAR_BOVEN,
+        NAAR_ONDER,
+        NAAR_LINKS,
+        NAAR_RECHTS
+    }
+}
---
+++ Sokoban/Model/Vakje.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sokoban.Model;
+
+namespace Sokoban.Model
+{
+    public class Vakje
+    {
+        private Soort soort;
+        private Dictionary<Richting, Vakje> buren = new Dictionary<Richting,Vakje>();
+        private Krat krat;
+
+        public Vakje(Soort soort)
+        {
+            this.soort = soort;
+            buren = null;
+        } 
+
+        public bool canMoveHere(Richting r) {
+            
+            return false;
+        }
+
+        public bool getNeighbours(Richting r) {
+            return false;
+        }
+
+        public bool hasKrat() {
+            if (krat != null)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        public void setNeighbour(){
+            
+        }  
+    }
+}
